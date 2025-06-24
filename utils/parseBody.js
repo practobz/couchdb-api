0ffd4e13dad20041ae9f953f4fd1acac15923f0a
Prachi Nadgouda <125897364+practobz@users.noreply.github.com>
2025-06-24 19:41:15 +0530

    Update parseBody.js

--- utils/parseBody.js
+++ utils/parseBody.js
@@ -2,9 +2,17 @@
 export async function parseBody(req) {
   return new Promise((resolve, reject) => {
     let body = '';
+    const MAX_SIZE = 1e6; // 1 MB limit (prevent abuse)
+
     req.on('data', chunk => {
       body += chunk.toString();
+
+      if (body.length > MAX_SIZE) {
+        reject(new Error('Request body too large'));
+        req.connection.destroy(); // end the stream
+      }
     });
+
     req.on('end', () => {
       try {
         const json = JSON.parse(body);
@@ -12,6 +20,10 @@ export async function parseBody(req) {
       } catch (err) {
         reject(new Error('Invalid JSON'));
       }
+    });
+
+    req.on('error', err => {
+      reject(new Error('Error reading request body: ' + err.message));
     });
   });
 }
